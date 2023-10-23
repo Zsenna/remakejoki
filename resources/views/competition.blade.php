2cd52c43e70345e242017a5d29329273025c1750
unknown <raid.abdurahman1@gmail.com>
2023-10-24 00:39:58 +0700

    first commit

---
+++ resources/views/competition.blade.php
@@ -0,0 +1,314 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="globals-comwork.css" />
+    <link rel="stylesheet" href="styleguide-comwork.css" />
+    <link rel="stylesheet" href="style-comwork.css" />
+    <link rel="stylesheet" href="navbar.css" />
+</head>
+
+<body>
+    <div class="education">
+        <div class="div">
+            <div class="overlap">
+                <div class="section">
+                    <div class="div-application">
+                        <div class="div-row">
+                            <div class="div-col-xs">
+                                <div class="text-wrapper">NEW! Product available now!</div>
+                                <div class="p-copytext">
+                                    <p class="docker-is-a-tool-for">
+                                        Docker is a tool for running applications and services in small, light-weight
+                                        &#34;containers&#34;
+                                        which do not interfere with your local machine&#39;s installed software or
+                                        configuration. This
+                                        means you don&#39;t have to worry about configuring or setting up complicated
+                                        development tools
+                                        such as web servers and databases on your local machine.
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="overlap-group">
+                                <img class="picture-key"
+                                    src="img/picture-key-automation-products-highres-en-1-fix696x391-webp-1.png" />
+                            </div>
+                        </div>
+                        <div class="link">
+                            <div class="text-wrapper-2">Find out more</div>
+                        </div>
+                    </div>
+                    <div class="text-wrapper-3">Competition</div>
+                    <footer class="footer">
+                        <div class="div-footer">
+                            <img class="div-footer-social" src="img/div-footer-social-wlq8h.svg" />
+                            <div class="div-footer-links">
+                                <div class="div-footer-links-2">
+                                    <div class="div-footer-links-3">
+                                        <div class="text-wrapper-4">Highlights</div>
+                                        <div class="div-2">
+                                            <div class="div-wrapper">
+                                                <div class="text-wrapper-5">Core product range</div>
+                                            </div>
+                                            <div class="link-2">
+                                                <div class="text-wrapper-5">Online Shop</div>
+                                            </div>
+                                            <div class="link-3">
+                                                <div class="text-wrapper-5">All product categories</div>
+                                            </div>
+                                            <div class="link-4">
+                                                <div class="text-wrapper-5">Southeast Asia Selected Products</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="div-footer-links-3">
+                                        <div class="text-wrapper-6">Contact</div>
+                                        <div class="div-2">
+                                            <div class="link-5">
+                                                <div class="text-wrapper-7">Get in touch</div>
+                                            </div>
+                                            <div class="link-5">
+                                                <div class="text-wrapper-5">Press portal</div>
+                                            </div>
+                                            <div class="link-5">
+                                                <div class="text-wrapper-5">Distributors</div>
+                                            </div>
+                                            <div class="link-6">
+                                                <div class="text-wrapper-5">Help and Support</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="div-footer-links-4">
+                                        <div class="text-wrapper-4">Industry segments</div>
+                                        <div class="div-2">
+                                            <div class="link-7">
+                                                <p class="text-wrapper-5">Automotive and tier 1 supplier industry</p>
+                                            </div>
+                                            <div class="link-8">
+                                                <div class="text-wrapper-5">Electronics industry</div>
+                                            </div>
+                                            <div class="link-9">
+                                                <div class="text-wrapper-5">Food processing and packaging</div>
+                                            </div>
+                                            <div class="link-10">
+                                                <p class="text-wrapper-5">Biotech, pharmaceutical and cosmetics
+                                                    industries</p>
+                                            </div>
+                                            <div class="link-11">
+                                                <div class="text-wrapper-5">Chemical industry</div>
+                                            </div>
+                                            <div class="link-12">
+                                                <div class="text-wrapper-5">Water technology</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="div-footer-legal-DP">
+                                <div class="div-left-column">
+                                    <p class="p">© 2023 PT. Festo All rights reserved</p>
+                                </div>
+                                <div class="list">
+                                    <div class="overlap-group-2">
+                                        <div class="item">
+                                            <div class="text-wrapper-8">Imprint</div>
+                                        </div>
+                                        <div class="link-data-protection-wrapper">
+                                            <div class="text-wrapper-8">Data protection</div>
+                                        </div>
+                                        <div class="link-terms-and-wrapper">
+                                            <div class="text-wrapper-8">Terms and conditions</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </footer>
+                </div>
+                <div class="div-fab-bar">
+                    <div class="button">
+                        <img class="SVG" src="img/svg.svg" />
+                        <div class="div-has-update"></div>
+                    </div>
+                    <img class="img" src="img/button.svg" />
+                </div>
+            </div>
+            <header class="header">
+                <div class="nav">
+                    <div class="div-navigation-aside">
+                        <img class="link-logo-blue-svg" src="img/link-logo-blue-svg.png" />
+                    </div>
+                    <div class="div-jsx-main">
+                        <div class="item-margin-wrapper">
+                            <div class="item-wrapper">
+                                <div class="list-wrapper">
+                                    <div class="list-2">
+                                        <div class="item-link">
+                                            <div class="text-wrapper-19">Corporate</div>
+                                            <!-- Submenu for Corporate -->
+                                            <div class="submenu">
+                                                <div class="subitem-link">
+                                                    <div class="text-wrapper-19">Experience</div>
+                                                    <!-- Grandchildren under "Experience" -->
+                                                    <div class="grandmenu">
+                                                        <div class="granditem-link" id="btn_highlight_akm">
+                                                            <a class="text-wrapper-19" href="/highlight">Highlight
+                                                                AKM</a>
+                                                        </div>
+                                                        <div class="granditem-link" id="btn_track_record">
+                                                            <a class="text-wrapper-19" href="/trackRecord">Track
+                                                                Record</a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="subitem-link">
+                                                    <div class="text-wrapper-19">About AKM</div>
+                                                    <!-- Grandchildren under "About KKM" -->
+                                                    <div class="grandmenu">
+                                                        <div class="granditem-link" id="btn_company_profile">
+                                                            <a class="text-wrapper-19" href="/compro">Company
+                                                                Profile</a>
+                                                        </div>
+                                                        <div class="granditem-link" id="btn_contact">
+                                                            <a class="text-wrapper-19" href="/contact">Contact</a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="item-link">
+                                <div class="text-wrapper-19">Education</div>
+                                <!-- Submenu for Highlight AKM -->
+                                <div class="submenu">
+                                    <div class="subitem-link">
+                                        <div class="text-wrapper-19">Training</div>
+                                        <!-- Grandchildren under "Highlight AKM" -->
+                                        <div class="grandmenu">
+                                            <div class="granditem-link" id="btn_training_center">
+                                                <a class="text-wrapper-19" href="/training">Training Center</a>
+                                            </div>
+                                            <div class="granditem-link" id="btn_onsite_training">
+                                                <a class="text-wrapper-19" href="/training#">Onsite Training</a>
+                                            </div>
+                                            <div class="granditem-link" id="btn_online_training">
+                                                <a class="text-wrapper-19" href="/training#">Online Training</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="subitem-link">
+                                        <div class="text-wrapper-19">Competition</div>
+                                        <!-- Grandchildren under "Track Record" -->
+                                        <div class="grandmenu">
+                                            <div class="granditem-link" id="">
+                                                <a class="text-wrapper-19" href="/competition">Foto dan Penjelasan
+                                                    Singkat</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="subitem-link">
+                                        <div class="text-wrapper-19">WORKSHOP & SEMINAR</div>
+                                        <!-- Grandchildren under "Track Record" -->
+                                        <div class="grandmenu">
+                                            <div class="granditem-link" id="">
+                                                <a class="text-wrapper-19" href="/worksem">Foto dan Penjelasan
+                                                    Singkat</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="subitem-link">
+                                        <div class="text-wrapper-19">PRODUCTS</div>
+                                        <!-- Grandchildren under "PRODUCTS" -->
+                                        <div class="grandmenu">
+                                            <div class="granditem-link">
+                                                <div class="text-wrapper-19">ROBOT</div>
+                                                <!-- Great-grandchildren under "ROBOT" -->
+                                                <div class="greatgrandmenu">
+                                                    <div class="granditem-link" id="btn_katalog_1">
+                                                        <a class="text-wrapper-19" href="/robot">Katalog</a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="granditem-link">
+                                                <div class="text-wrapper-19">AGV</div>
+                                                <div class="greatgrandmenu">
+                                                    <div class="granditem-link" id="btn_katalog_2">
+                                                        <a class="text-wrapper-19" href="/agv">Katalog</a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="granditem-link">
+                                                <div class="text-wrapper-19">PLC TRAINER</div>
+                                                <div class="greatgrandmenu">
+                                                    <div class="granditem-link" id="btn_katalog_3">
+                                                        <a class="text-wrapper-19" href="/plc">Katalog</a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="granditem-link">
+                                                <div class="text-wrapper-19">MECLAB NON PLC</div>
+                                                <div class="greatgrandmenu">
+                                                    <div class="granditem-link" id="btn_katalog_4">
+                                                        <a class="text-wrapper-19" href="/nplc">Katalog</a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+
+                                </div>
+                            </div>
+                            <div class="item-link">
+                                <div class="text-wrapper-19">Industrial</div>
+                                <div class="submenu">
+                                    <div class="subitem-link">
+                                        <div class="text-wrapper-19" id="btn_si">SI</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="div-search-field"></div>
+            </header>
+            <div class="div-row-2">
+                <div class="link-13">
+                    <div class="div-accordion">
+                        <img class="picture-energy"
+                            src="img/picture-energy-saving-services-screen-fix496x661-webp-1.png" />
+                    </div>
+                    <div class="text-wrapper-10">Energy Saving Services</div>
+                </div>
+                <div class="link-14">
+                    <div class="div-accordion">
+                        <img class="picture-energy"
+                            src="img/picture-energy-saving-services-screen-fix496x661-webp-2.png" />
+                    </div>
+                    <div class="text-wrapper-11">Digitalisation</div>
+                </div>
+                <div class="link-13">
+                    <div class="div-accordion">
+                        <img class="picture-energy"
+                            src="img/picture-energy-saving-services-screen-fix496x661-webp-1.png" />
+                    </div>
+                    <div class="text-wrapper-10">Energy Saving Services</div>
+                </div>
+                <div class="link-15">
+                    <div class="link-16">
+                        <div class="div-accordion">
+                            <img class="picture-energy"
+                                src="img/picture-energy-saving-services-screen-fix496x661-webp.png" />
+                        </div>
+                        <div class="text-wrapper-10">Energy Saving Services</div>
+                    </div>
+                    <div class="text-wrapper-12">Highlight AKM</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
